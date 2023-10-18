0a5a7402efe53b0142e9e68621d1d89174d8caca
1ekso <chuliganas9@gmail.com>
2023-10-18 13:57:01 +0300

    updated PD4

--- PD4/main.js
+++ PD4/main.js
@@ -1,6 +1,4 @@
-var petName = "Baris";
-var petAge = 5;
-console.log(petName);
-console.log(petAge);
-const buildingAddress = `${Dauksto gatve 9}`;
-console.log(buildingAddress);
+let petName = "Baris";
+let petAge = 5;
+const buildingAddress = "Dauksto 9";
+console.log(`My pet is named ${petName} and is ${petAge} years old. He lives in ${buildingAddress} street.`);
