f828c02b3381653095576498dfc2b9397a3d8c7e
reapon101 <Kibbutsu@proton.mail>
2023-11-09 05:09:30 +0200

    test

--- PD4/main.js
+++ PD4/main.js
@@ -1,4 +1,5 @@
 let petName = "Baris";
 let petAge = 5;
+
 const buildingAddress = "Dauksto 9";
 console.log(`My pet is named ${petName} and is ${petAge} years old. He lives in ${buildingAddress} street.`);
