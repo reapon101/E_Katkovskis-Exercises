cc05ae03e7c8a0e2ea96edabb74d55ccc6006b62
Emanuelis <kibbutsu@proton.me>
2023-11-29 15:13:24 +0200

    updated

--- PD12/PD12.1/js/main.js
+++ PD12/PD12.1/js/main.js
@@ -1,29 +1,15 @@
-<<<<<<< HEAD
-// Paskaitu kiekis kiekviena savaite:
-let paskaitukiekis = {
-    pirmadienis: 5,
-    antradienis: 6,
-    trečiadinis: 4,
-    ketvirtadienis: 5,
-    penktadienis: 4,
-=======
 // Paskaitu kiekis
 let paskaitukiekis = {
-pirmadieni: 5,
-antradieni: 6,
-trečiadini: 4,
-ketvirtadieni: 5,
-penktadieni: 4,
->>>>>>> 7c87fc74959dd225babf22d51e1534f3248e2a62
+    pirmadieni: 5,
+    antradieni: 6,
+    trečiadini: 4,
+    ketvirtadieni: 5,
+    penktadieni: 4,
 }
 
 // Kiek paskaitų turi jis per savaitę?
 paskaitukiekis = 5 + 6 + 4 + 5 + 4;
-<<<<<<< HEAD
 console.log(paskaitukiekis + " paskaitos per savaitę");
-=======
-
->>>>>>> 7c87fc74959dd225babf22d51e1534f3248e2a62
 
 // Kiek tai sudaro minučių? Viena paskaita trunka 45 minutes.
 let paskaitulaikas = paskaitukiekis * 45;
--- PD12/PD12.3/js/main.js
+++ PD12/PD12.3/js/main.js
@@ -11,7 +11,6 @@ let plytosAukstis = 10;
 let plytosplotas = plytosIlgis * plytosAukstis;
 console.log(plytosplotas); // 200
 
-<<<<<<< HEAD
 // Kiek plytu reikes?
 let plytuskaicius = sienosplotas / plytosplotas;
 console.log(plytuskaicius); // 600 plytu
@@ -19,8 +18,3 @@ console.log(plytuskaicius); // 600 plytu
 // Kiek tu plytu kainuos?
 let plytukaina = plytuskaicius * plytoskaina;
 console.log(plytukaina.toPrecision(5));
-=======
-let plytureikes = sienosplotas / plytosplotas;
-let plytoskainuos = plytureikes * plytkaina;
-console.log("PLytu reikes " + plytureikes + " vienetu" + ", kurios kainuos " + plytoskainuos + " Eur");
->>>>>>> 7c87fc74959dd225babf22d51e1534f3248e2a62
